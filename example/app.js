3a1cf7ec1362819ed19d9b9f0054001632499638
mannynotfound <manny@art404.com>
2016-05-16 04:23:35 +0000

    caching attempts

--- example/app.js
+++ example/app.js
@@ -5,36 +5,53 @@ import accounts from './accounts'
 import jsonfile from 'jsonfile'
 import path from 'path'
 
+var tweetCache = []
+var saving = false
+
+if (process.env.SILENT) {
+  console.log = () => {}
+}
+
 function supremeCallback(err, data, cfg) {
   if (err) {
     console.log('shit hit the fan'.red.bold)
     console.log(JSON.stringify(err, 0, 2).red)
     console.log('')
   } else {
-    console.log(cfg.label.grey)
+    if (!data.user || !data.text) return
     console.log('~~~~~~~~~~~~~~~~~~~~~~'.grey)
     console.log('')
-    console.log(data.text.cyan.bold)
-    console.log('')
+    const label = `${cfg.label} |`.grey
+    const name = `@${data.user.screen_name}:`.yellow.bold
+    const text = data.text.cyan.bold
+    console.log(`${label} ${name} ${text}`)
     console.log('')
 
-    let tweets = []
     const output = path.resolve(__dirname, '/') + `mnt/data/${cfg.label.toLowerCase().replace(/ /g, '-')}/tweets.json`
 
-    try {
-      tweets = require(output)
-    } catch(e) {
+    tweetCache.push(data)
+
+    if (tweetCache.length > 99 && !saving) {
+      saving = true
+
+      try {
+        tweetCache = require(output).concat(tweetCache)
+      } catch(e) {
+        console.log('COULDNT REQUIRE TWEETS')
+      }
+
+      jsonfile.writeFile(output, tweetCache, {spaces: 2}, (err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          tweetCache.splice(0, 99)
+        }
+        saving = false
+      })
     }
 
-    tweets.push(data)
-
-    jsonfile.writeFile(output, tweets, {spaces: 2}, (err) => {
-      if (err) {
-        console.log(err)
-      }
-    })
   }
 }
 
-const SupremeStream = require('../src/supreme-stream')
+const SupremeStream = require('../lib/supreme-stream')
 new SupremeStream(streams, accounts, supremeCallback).startAll()
